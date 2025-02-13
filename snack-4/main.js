c90beb552f89469fa1b7d3d20a1b939b24dd5dc9
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-13 12:48:31 +0100

    add snack 4

---
+++ snack-4/main.js
@@ -0,0 +1,17 @@
+/* Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. */
+
+let num = [];
+
+for (let i = 0; i < 6; i++) {
+    const userNumber = Number(prompt("Inserisci un numero"))
+    console.log(userNumber);
+    if (userNumber % 2 == 0) {
+        console.log(`${userNumber} is even`);
+    }
+    else {
+        num.push(userNumber)
+    }
+}
+console.log(`${num} are odd`);
