053bc9c7662158e6f1e7cef624785a73a6a38c8e
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-02-13 10:29:12 +0100

    first commit

---
+++ snack-1/main.js
@@ -0,0 +1,7 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+const userNumber = Number(prompt("Inserisci un numero"));
+const userNumberAgain = Number(prompt("Inserisci un altro numero"));
+
+if ()
